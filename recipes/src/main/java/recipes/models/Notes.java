f4268903d58f516b47ad4037dc7a6ab9ae13deed
maja-wirkijowska <97481772+maja-wirkijowska@users.noreply.github.com>
2022-05-13 07:49:15 -0700

    created Recipe and Notes models

---
+++ recipes/src/main/java/recipes/models/Notes.java
@@ -0,0 +1,52 @@
+package recipes.models;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne // no cascade here, if we delete notes, recipe stays
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+
+
+
+
+
+
+
+
+
+
+    // getters/setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
